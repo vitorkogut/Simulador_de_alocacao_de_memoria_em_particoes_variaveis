2455a7cb045e42a3aa446039950bd8980ffd0139
João Vitor Specht Kogut <49065277+vitorkogut@users.noreply.github.com>
2020-05-25 10:12:03 -0300

    V 1.0

---
+++ src/simuladormemoria/Bloco_processo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuladormemoria;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author vitor
+ */
+public class Bloco_processo {
+    public int cod;
+    public int tempo;
+    public int tamanho;
+    public String nome;
+    public int cor;
+    public float tamnhoEmCelulas;
+    public boolean inserido;
+    public int inicio;
+    public int fim;
+    
+    
+    
+}
